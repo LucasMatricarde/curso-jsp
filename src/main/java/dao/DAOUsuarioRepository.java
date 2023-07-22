28a24f15142a8c9646cc31d7979d89a1b1726a29
Lucas Matricarde <lucasmatricarde@hotmail.com>
2023-07-22 09:11:50 -0300

    primeiro commit, agora sempre colocando no git

---
+++ src/main/java/dao/DAOUsuarioRepository.java
@@ -0,0 +1,289 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import connection.SingleConnectionBanco;
+import model.ModelLogin;
+
+public class DAOUsuarioRepository {
+	
+	
+	private Connection connection;
+	
+	
+	public DAOUsuarioRepository() {
+		connection = SingleConnectionBanco.getConnection();
+	}
+	
+	public ModelLogin gravarUsuario(ModelLogin objeto, Long usuarioLogado) throws Exception {
+		
+		if(objeto.isNovo()) {//Grava um novo
+			String sql = "INSERT INTO model_login(login, senha, nome, email, usuario_id, perfil, sexo, cep, logradouro, numero, bairro, localidade, estado)  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			
+			statement.setString(1, objeto.getLogin());
+			statement.setString(2, objeto.getSenha());
+			statement.setString(3, objeto.getNome());
+			statement.setString(4, objeto.getEmail());
+			statement.setLong(5, usuarioLogado);
+			statement.setString(6, objeto.getPerfil());
+			statement.setString(7, objeto.getSexo());
+			statement.setString(8, objeto.getCep());
+			statement.setString(9, objeto.getLogradouro());
+			statement.setString(10, objeto.getNumero());
+			statement.setString(11, objeto.getBairro());
+			statement.setString(12, objeto.getLocalidade());
+			statement.setString(13, objeto.getEstado());
+			statement.execute();
+			
+			connection.commit();
+			
+			if(objeto.getFotoUser() != null && !objeto.getFotoUser().isEmpty()) {
+				sql = "UPDATE model_login set fotouser = ?, extensaofotouser = ? WHERE login = ?";
+				statement = connection.prepareStatement(sql);
+				statement.setString(1, objeto.getFotoUser());
+				statement.setString(2, objeto.getExtensaoFotoUser());
+				statement.setString(3, objeto.getLogin());
+				statement.execute();
+				
+				connection.commit();
+			}
+			
+		}else {// atualizar
+			String sql = "UPDATE model_login SET login = ?, senha = ?, nome = ?, email = ?, perfil = ?, sexo = ?, cep = ?, logradouro = ?, numero = ?, bairro = ?, localidade = ?, estado = ? WHERE id = "+objeto.getId()+";";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			
+			statement.setString(1, objeto.getLogin());
+			statement.setString(2, objeto.getSenha());
+			statement.setString(3, objeto.getNome());
+			statement.setString(4, objeto.getEmail());
+			statement.setString(5, objeto.getPerfil());
+			statement.setString(6, objeto.getSexo());
+			statement.setString(7, objeto.getCep());
+			statement.setString(8, objeto.getLogradouro());
+			statement.setString(9, objeto.getNumero());
+			statement.setString(10, objeto.getBairro());
+			statement.setString(11, objeto.getLocalidade());
+			statement.setString(12, objeto.getEstado());
+			
+			statement.executeUpdate();
+			
+			connection.commit();
+			
+			if(objeto.getFotoUser() != null && !objeto.getFotoUser().isEmpty()) {
+				sql = "UPDATE model_login set fotouser = ?, extensaofotouser = ? WHERE id = ?";
+				statement = connection.prepareStatement(sql);
+				statement.setString(1, objeto.getFotoUser());
+				statement.setString(2, objeto.getExtensaoFotoUser());
+				statement.setLong(3, objeto.getId());
+				statement.execute();
+				
+				connection.commit();
+			}
+		}
+		
+		return this.consultaUsuario(objeto.getLogin(), usuarioLogado);
+	}
+	
+	public List<ModelLogin> consultaUsuarioList(String nome, Long userLogado) throws Exception {
+		List<ModelLogin> retorno = new ArrayList<ModelLogin>();
+		String sql = "SELECT * FROM model_login WHERE UPPER(nome) LIKE UPPER(?) AND useradmin is false AND usuario_id = "+userLogado;
+		PreparedStatement statement = connection.prepareStatement(sql);
+		
+		statement.setString(1, "%" + nome + "%");
+		statement.setLong(2, userLogado);
+		ResultSet resultado =  statement.executeQuery();
+		
+		while (resultado.next()) /*Se tem resultado*/ {
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setId(resultado.getLong("id"));
+			modelLogin.setNome(resultado.getString("nome"));
+			modelLogin.setEmail(resultado.getString("email"));
+			modelLogin.setLogin(resultado.getString("login"));
+			modelLogin.setPerfil(resultado.getString("perfil"));
+			modelLogin.setSexo(resultado.getString("sexo"));
+			//modelLogin.setSenha(resultado.getString("senha"));
+			
+			retorno.add(modelLogin);
+		}
+		
+		return retorno;
+	}
+	
+	public List<ModelLogin> consultaUsuarioList(Long userLogado) throws Exception {
+		List<ModelLogin> retorno = new ArrayList<ModelLogin>();
+		String sql = "SELECT * FROM model_login WHERE useradmin is false AND usuario_id = "+userLogado+" ORDER BY id";
+		PreparedStatement statement = connection.prepareStatement(sql);
+		
+		ResultSet resultado =  statement.executeQuery();
+		
+		while (resultado.next()) /*Se tem resultado*/ {
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setId(resultado.getLong("id"));
+			modelLogin.setNome(resultado.getString("nome"));
+			modelLogin.setEmail(resultado.getString("email"));
+			modelLogin.setLogin(resultado.getString("login"));
+			modelLogin.setPerfil(resultado.getString("perfil"));
+			modelLogin.setSexo(resultado.getString("sexo"));
+			//modelLogin.setSenha(resultado.getString("senha"));
+			
+			retorno.add(modelLogin);
+		}
+		
+		return retorno;
+	}
+	
+	public ModelLogin consultaUsuario(String login) throws Exception  {
+		
+		ModelLogin modelLogin = new ModelLogin();
+		
+		String sql = "select * from model_login where upper(login) = upper('"+login+"') AND useradmin is false";
+		
+		PreparedStatement statement = connection.prepareStatement(sql);
+		
+		ResultSet resultado =  statement.executeQuery();
+		
+		while (resultado.next()) /*Se tem resultado*/ {
+			
+			modelLogin.setId(resultado.getLong("id"));
+			modelLogin.setEmail(resultado.getString("email"));
+			modelLogin.setLogin(resultado.getString("login"));
+			modelLogin.setSenha(resultado.getString("senha"));
+			modelLogin.setNome(resultado.getString("nome"));
+			modelLogin.setPerfil(resultado.getString("perfil"));
+			modelLogin.setSexo(resultado.getString("sexo"));
+		}
+		
+		
+		return modelLogin;
+		
+	}
+	
+public ModelLogin consultaUsuarioLogado(String login) throws Exception  {
+		
+		ModelLogin modelLogin = new ModelLogin();
+		
+		String sql = "select * from model_login where upper(login) = upper('"+login+"')";
+		
+		PreparedStatement statement = connection.prepareStatement(sql);
+		
+		ResultSet resultado =  statement.executeQuery();
+		
+		while (resultado.next()) /*Se tem resultado*/ {
+			
+			modelLogin.setId(resultado.getLong("id"));
+			modelLogin.setEmail(resultado.getString("email"));
+			modelLogin.setLogin(resultado.getString("login"));
+			modelLogin.setSenha(resultado.getString("senha"));
+			modelLogin.setNome(resultado.getString("nome"));
+			modelLogin.setUseradmin(resultado.getBoolean("useradmin"));
+			modelLogin.setPerfil(resultado.getString("perfil"));
+			modelLogin.setSexo(resultado.getString("sexo"));
+			modelLogin.setFotoUser(resultado.getString("fotoUser"));
+			modelLogin.setCep(resultado.getString("cep"));
+			modelLogin.setLogradouro(resultado.getString("logradouro"));
+			modelLogin.setNumero(resultado.getString("numero"));
+			modelLogin.setLocalidade(resultado.getString("localidade"));
+			modelLogin.setBairro(resultado.getString("bairro"));
+			modelLogin.setEstado(resultado.getString("estado"));
+		}
+		
+		
+		return modelLogin;
+		
+	}
+	
+	public ModelLogin consultaUsuario(String login, Long userLogado) throws Exception  {
+		
+		ModelLogin modelLogin = new ModelLogin();
+		
+		String sql = "select * from model_login where upper(login) = upper('"+login+"') AND useradmin is false AND usuario_id ="+userLogado;
+		
+		PreparedStatement statement = connection.prepareStatement(sql);
+		
+		ResultSet resultado =  statement.executeQuery();
+		
+		while (resultado.next()) /*Se tem resultado*/ {
+			
+			modelLogin.setId(resultado.getLong("id"));
+			modelLogin.setEmail(resultado.getString("email"));
+			modelLogin.setLogin(resultado.getString("login"));
+			modelLogin.setSenha(resultado.getString("senha"));
+			modelLogin.setNome(resultado.getString("nome"));
+			modelLogin.setPerfil(resultado.getString("perfil"));
+			modelLogin.setSexo(resultado.getString("sexo"));
+			modelLogin.setFotoUser(resultado.getString("fotoUser"));
+			modelLogin.setCep(resultado.getString("cep"));
+			modelLogin.setLogradouro(resultado.getString("logradouro"));
+			modelLogin.setNumero(resultado.getString("numero"));
+			modelLogin.setLocalidade(resultado.getString("localidade"));
+			modelLogin.setBairro(resultado.getString("bairro"));
+			modelLogin.setEstado(resultado.getString("estado"));
+		}
+		
+		
+		return modelLogin;
+		
+	}
+	
+	public ModelLogin consultaUsuarioId(String id, Long userLogado) throws Exception  {
+		
+		ModelLogin modelLogin = new ModelLogin();
+		
+		String sql = "select * from model_login where id = ? AND useradmin is false AND usuario_id = ?";
+		
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setLong(1, Long.parseLong(id));
+		statement.setLong(2, userLogado);
+		ResultSet resultado =  statement.executeQuery();
+		
+		while (resultado.next()) /*Se tem resultado*/ {
+			
+			modelLogin.setId(resultado.getLong("id"));
+			modelLogin.setEmail(resultado.getString("email"));
+			modelLogin.setLogin(resultado.getString("login"));
+			modelLogin.setSenha(resultado.getString("senha"));
+			modelLogin.setNome(resultado.getString("nome"));
+			modelLogin.setPerfil(resultado.getString("perfil"));
+			modelLogin.setSexo(resultado.getString("sexo"));
+			modelLogin.setFotoUser(resultado.getString("fotouser"));
+			modelLogin.setExtensaoFotoUser(resultado.getString("extensaofotouser"));
+			modelLogin.setCep(resultado.getString("cep"));
+			modelLogin.setLogradouro(resultado.getString("logradouro"));
+			modelLogin.setNumero(resultado.getString("numero"));
+			modelLogin.setLocalidade(resultado.getString("localidade"));
+			modelLogin.setBairro(resultado.getString("bairro"));
+			modelLogin.setEstado(resultado.getString("estado"));
+		}
+		
+		
+		return modelLogin;
+		
+	}
+	
+	public boolean validarLogin(String login) throws Exception {
+		String sql = "select count(1) > 0 as existe from model_login where upper(login) = upper('"+login+"');";
+		
+        PreparedStatement statement = connection.prepareStatement(sql);
+		
+		ResultSet resutlado =  statement.executeQuery();
+		
+		resutlado.next();/*Pra ele entrar nos resultados do sql*/
+		return resutlado.getBoolean("existe");
+		
+	}
+	
+	public void deletarUser(String idUser) throws Exception {
+		String sql = "DELETE FROM model_login WHERE id = ? AND useradmin is false";
+		
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setLong(1, Long.parseLong(idUser));
+		statement.executeUpdate();
+		connection.commit();
+	}
+	
+}
---
+++ src/main/java/model/ModelLogin.java
@@ -0,0 +1,147 @@
+package model;
+
+import java.io.Serializable;
+
+public class ModelLogin implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	private String email;
+	private String login;
+	private String senha;
+	private boolean useradmin;
+	private String perfil;
+	private String sexo;
+	private String fotoUser;
+	private String extensaoFotoUser;
+	private String cep;
+	private String logradouro;
+	private String bairro;
+	private String localidade;
+	private String estado;
+	private String numero;
+	
+	public boolean isNovo() {
+		if(this.id == null) {
+			return true;
+		}
+		return false;
+	}
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public boolean isUseradmin() {
+		return useradmin;
+	}
+	
+	public void setUseradmin(boolean useradmin) {
+		this.useradmin = useradmin;
+	}
+	
+	public String getPerfil() {
+		return perfil;
+	}
+	
+	public void setPerfil(String perfil) {
+		this.perfil = perfil;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+	
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	public String getFotoUser() {
+		return fotoUser;
+	}
+	
+	public void setFotoUser(String fotoUser) {
+		this.fotoUser = fotoUser;
+	}
+	
+	public String getExtensaoFotoUser() {
+		return extensaoFotoUser;
+	}
+	
+	public void setExtensaoFotoUser(String extensaoFotoUser) {
+		this.extensaoFotoUser = extensaoFotoUser;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getLocalidade() {
+		return localidade;
+	}
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+}
